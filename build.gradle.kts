5ff2c065a39f977b69e9bfaf9954085b139cadcb
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2025-02-21 16:09:07 -0600

    Simpler

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,28 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.5.0-M2"
-	id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "hm.binkley"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("com.github.stefanbirkner:system-lambda:1.2.1")
 }
 
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
-}
-
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
